910c06b2364677dff453921d19c2484eb20c2051
Saúl García <alu0101129785@ull.edu.es>
2021-03-05 19:04:39 +0000

    Ejercicio 5

--- exercises/src/index.ts
+++ exercises/src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hola mundo");
+//console.log("Hola mundo");
+function Chuck_Norris() {
+  
+}
+
+var cadena = "Test abc test test abc test test test abc test test abc";
+var re = /abc/g;
+var resultado = cadena.replace(re, '');
+console.log(resultado);
