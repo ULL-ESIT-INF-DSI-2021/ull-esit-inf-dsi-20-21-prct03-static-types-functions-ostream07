ce319f01df8790d80a4b8a75c4d3bc116089e2fa
Saúl García <alu0101129785@ull.edu.es>
2021-03-07 17:21:37 +0000

    commit

--- exercises/src/index.ts
+++ exercises/src/index.ts
@@ -1,34 +1,12 @@
 //console.log("Hola mundo");
-function Chuck_Norris(punch) {
-  var without_a: string = "";
-  var without_e: string = "";
-  var range: string = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
-  var range_size: number = range.length;
-  without_a = punch.replace(delete_a, '');
-  without_e = without_a.replace(delete_e, '');
-  var compare: string = "";
-  var by_order: string = "";
-  var concat: string = "";
-  var size: number = without_e.length;
-  //console.log(without_e);
-  
-  for(var i: number = 0; i < size; i++) {
-    for (var j: number = 0; j < range_size; j++){
-      if(without_e.charAt(i) == range.charAt(j)) {
-        compare = range.charAt(j);
-        if(compare.match(range)) {
-          var hola = compare.indexOf(range);
-        }
-        console.log(hola);
-      }
-    }
+function ipsInRange(ip1, ip2) {
+  var arr = ip1.split(".");
+  for(var i: number = 0; i < ip1.length; i++) {
+    console.log(arr(i));
   }
 }
 
-var punch: string = "Beard Jeans Hairbrush Knuckleduster Sand";
-var delete_a = /a/g;
-var delete_e = /e/g;
+var ip1: string = "10.0.0.0";
+var ip2: string = "10.0.0.50";
 
-//var resultado = punch.replace(delete_a, '');
-//console.log(resultado);
-Chuck_Norris(punch);
+ipsInRange(ip1, ip2);
